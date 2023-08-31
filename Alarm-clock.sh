22831737804d616aa4ea0a6b440ea4407e53b481
Tayyab-R <riaztayyab212@gmail.com>
2023-08-31 20:26:02 +0500

    Alarm clock without sound

--- Alarm-clock.sh
+++ Alarm-clock.sh
@@ -11,27 +11,44 @@ validate_alarm_time() {
     fi
 }
 
+trigger_alarm_by_message() {
+    $1
+    $2
+    if [ "$1" \> "$2" ]
+    then
+        echo "Allarm will be triggerred by message...."
+    else
+        echo "Alarm is triggered"
+        
+    fi
+}
+
+trigger_alarm_by_sound() {
+    echo "The feature of triggering alarm by sound is in the work"
+    echo "Please wait. Thank you for you patience."
+}
+
 read -rp  "Enter time to set alarm (HH:MM): " alarm_time
-# read -rp "Enter the way you want the alarm to be triggered: 1(alarm message) 2(alarm sound)" alarm_trigger
+read -rp "Enter the way you want the alarm to be triggered: 1(alarm message) 2(alarm sound)" alarm_trigger
 
 
-# validate input time format
-validate_alarm_time "$alarm_time"
+
 
 
 while true
 do
-    current_time=$(date +%R)
-    if [ "$alarm_time" \> "$current_time" ]
-    then
-        echo "Alarm will be triggered"
-        sleep 1
-        date +%R
-        
-    else
-        echo "Alarm is triggered"
-        date +%R
-        break
+    # validate input time format
+    validate_alarm_time "$alarm_time"
 
+    current_time=$(date +%R)
+    if [[ "$alarm_trigger" = 1 ]]
+    then
+        trigger_alarm_by_message "$alarm_time" "$current_time" 
+        break
+    else
+        trigger_alarm_by_sound
+        break
+        
     fi
+    
 done
